82722630d5d83d66142d7af9811820b6254a571e
Sangmin Yoon <sanspareilsmyn@gmail.com>
2025-04-15 19:38:29 +0900

    Add kforward formula 0.1.1

--- Formula/kforward.rb
+++ Formula/kforward.rb
@@ -2,8 +2,8 @@
 class Kforward < Formula
   desc "Port-forwarding helper for Kubernetes services using kubectl"
   homepage "https://github.com/sanspareilsmyn/kforward"
-  url "https://github.com/sanspareilsmyn/kforward/archive/refs/tags/0.1.0.tar.gz"
-  sha256 "630eae2778efbce76f3ca33cf2248e71b11c335264147b38a714bdd3666475ec"
+  url "https://github.com/sanspareilsmyn/kforward/archive/refs/tags/0.1.1.tar.gz"
+  sha256 "b15bd3f2b12120a669c3ea01d53fac473cea56b2bd31fb7784d6e9f61ed4338a"
   license "Apache License 2.0"
 
   depends_on "go" => :build
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # homebrew-kforward
+
+- curl -LO https://github.com/sanspareilsmyn/kforward/archive/refs/tags/0.1.1.tar.gz
+- shasum -a 256 0.1.1.tar.gz
